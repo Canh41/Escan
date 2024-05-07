cf820c86ac1c201b250c2330a385af5fef8c9559
Nguyen Cong Anh <nguyenconganh0401@gmail.com>
2024-05-07 14:48:19 +0700

    Base tool scan vuln

---
+++ Src/main/java/Entity/robot_file_module.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * 
+ */
+public class robot_file_module {
+
+    private String User_agent = "";
+    private List<String> Allow = new ArrayList<String>();
+    private List<String> Disallow = new ArrayList<String>();
+
+    public robot_file_module() {
+    }
+    public robot_file_module(String user_agent, List<String> allow, List<String> disallow) {
+        User_agent = user_agent;
+        Allow = allow;
+        Disallow = disallow;
+    }
+    
+
+    public String getUser_agent() {
+        return User_agent;
+    }
+
+    public void setUser_agent(String User_agent) {
+        this.User_agent = User_agent;
+    }
+
+    public List<String> getAllow() {
+        return Allow;
+    }
+
+    public void setAllow(List<String> Allow) {
+        this.Allow = Allow;
+    }
+
+    public List<String> getDisallow() {
+        return Disallow;
+    }
+
+    public void setDisallow(List<String> Disallow) {
+        this.Disallow = Disallow;
+    }
+
+    @Override
+    public String toString() {
+        return "robot_file_module{" + "User_agent=" + User_agent + ", Allow=" + Allow + ", Disallow=" + Disallow + '}';
+    }
+
+}
