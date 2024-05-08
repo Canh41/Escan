789b5f2b86099cef388a87ce092392d9835ed507
Nguyen Cong Anh <nguyenconganh0401@gmail.com>
2024-05-08 15:01:54 +0700

    Add signature

---
+++ Src/main/java/PayloadSignature/A4InsecureDesign/sensitive_file.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PayloadSignature.A4InsecureDesign;
+
+/**
+ *
+ * 
+ */
+public class sensitive_file {
+
+    private String[] senFile;
+
+    public sensitive_file() {
+        senFile = new String[]{
+            "sql",
+            "zip",
+            "rar",
+            "7z",
+            "bak",
+            "log",
+            "mail",
+            "txt",
+            "dump",};
+    }
+
+    public String[] getSenFile() {
+        return senFile;
+    }
+}
---
+++ Src/main/java/PayloadSignature/A5SecurityMisconfiguration/Backup_And_Unreferenced_Files.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PayloadSignature.A5SecurityMisconfiguration;
+
+/**
+ *
+ * 
+ */
+public class Backup_And_Unreferenced_Files {
+
+    
+    private String payloa = "";
+
+    public Backup_And_Unreferenced_Files() {
+    }
+
+    public String getPayloa() {
+        return payloa;
+    }
+
+    public void setPayloa(String payloa) {
+        this.payloa = payloa;
+    }
+
+   
+
+    public String getPayload(String domain) {
+        payloa = "https://www.google.com.vn/search?q=site%3A%22" + domain + "%22+intext%3A%28bak%7Csql%7Czip%7Cdump%7Ctxt%7Crar%7C7z%7Cbackp%29";
+        return payloa;
+    }
+}
